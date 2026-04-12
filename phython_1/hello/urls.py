a0ba8c1b986a911202cf59de745f801a88123a96
ben <bernhard.peter@hotmail.com>
2026-04-12 13:21:56 +0200

    Erstes Phyton-Projekt mit Django

---
+++ phython_1/hello/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.hello_world),
+]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.hello_world),
+]
